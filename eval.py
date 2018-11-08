e196cd825b0f4bd215c02c5096f80c360cbec913
JiazeWang <jiazewang@csu.edu.cn>
2018-11-08 16:53:52 +0800

    fix

--- eval.py
+++ eval.py
@@ -5,8 +5,8 @@ from dice_loss import dice_coeff
 
 
 def jaccard_distance(y_true, y_pred):
-    intersection = torch.sum(torch.abs(y_true * y_pred), axis=-1)
-    sum_ = torch.sum(torch.abs(y_true) + torch.abs(y_pred), axis=-1)
+    intersection = torch.sum(torch.abs(y_true * y_pred), dim=-1)
+    sum_ = torch.sum(torch.abs(y_true) + torch.abs(y_pred), dim=-1)
     jac = (intersection) / (sum_ - intersection)
     return jac
 
